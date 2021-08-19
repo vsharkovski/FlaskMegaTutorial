93f401c461fb8da763474b101bd3130f9fbb1517
Vladimir Sharkovski <vsarkovskii@gmail.com>
2021-08-19 19:25:27 +0200

    Add config, simple Login view and template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
 
 @app.route("/")
 @app.route("/index")
@@ -15,4 +17,14 @@ def index():
             "body": "I am a Sick Dog. They made me into the Devouring Song."
         }
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # POST, form was submitted and correctly validated
+        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+        return redirect(url_for("index"))
+    return render_template("login.html", form=form)
