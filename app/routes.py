5db45fb3012153f2315501dc206a937606317984
Vladimir Sharkovski <vsarkovskii@gmail.com>
2021-08-19 18:28:05 +0200

    chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world!"
+    user = {"username": "Vlatko"}
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "I am a Dog of War."
+        },
+        {
+            "author": {"username": "Yalda"},
+            "body": "I am a Sick Dog. They made me into the Devouring Song."
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
