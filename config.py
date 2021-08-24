53ccd0f1cc487d65b14efdb4aca9bdb7c635a593
Vladimir Sharkovski <vsarkovskii@gmail.com>
2021-08-24 18:08:27 +0200

    make it so unnecessary things are not synced with git

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-        "sqlite:///" + os.path.join(basedir, "app.db")
+        "sqlite:///" + os.path.join(basedir, "instance", "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
@@ -13,4 +13,4 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = ["devvlado@gmail.com"]
+    ADMINS = [os.environ.get("MAIL_ADMIN_ADDRESS")]
