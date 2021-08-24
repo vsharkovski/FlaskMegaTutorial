5d42728380eb3b030e930df999644cd9e7e6ec1d
Vladimir Sharkovski <vsarkovskii@gmail.com>
2021-08-24 17:56:12 +0200

    added email on error, log info to file; fixed same username error in edit_profile

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 from config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,4 +18,43 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 
-from app import routes, models
+if not app.debug:
+    # Configure so errors are sent to admin emails
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+            toaddrs=app.config["ADMINS"], subject="Microblog Failure",
+            credentials=auth,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR) # only send errors
+        app.logger.addHandler(mail_handler)
+
+    # Logging to a file
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    
+    file_handler = RotatingFileHandler(
+        os.path.join("logs", "microblog.log"),
+        maxBytes=10420,
+        backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        "%(asctime)s %(levelname):s %(message)s [in %(pathname):s%(lineno)d]"))
+    file_handler.setLevel(logging.INFO) # send most things
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+
+    # Every time the server is started we log the following
+    app.logger.info("Microblog startup")
+
+
+from app import routes, models, errors
---
+++ app/errors.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template(
+        "error.html",
+        error_header="File not found",
+        error_message=""
+    ), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template(
+        "error.html",
+        error_header="An unexpected error has occured",
+        error_message="The administrator has been notified. Sorry for the inconvenience!"
+    ), 500
